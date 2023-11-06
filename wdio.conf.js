46952256858abe3a1ddb1b194cde34a69a334169
saidmalik0v <saidmalikov1911@gmail.com>
2023-11-06 21:45:24 +0500

    report updates

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        '/registration.js'
+        './test/registration.js'
     ],
     // Patterns to exclude.
     exclude: [
